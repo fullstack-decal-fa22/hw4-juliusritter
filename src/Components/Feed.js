15a4ed63339d5934abc23cf53c9acd0cdd7e706a
juliusritter <111826932+juliusritter@users.noreply.github.com>
2022-11-30 14:38:50 -0800

    Update Feed.js

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -6,18 +6,20 @@ import Block from './Block';
 const Feed = () => {
     /* TODO: Declare a new state variable to keep track of the blocks on your Blockstagram feed! */
     // Refer to Hint 2 for more help!
-
+    const [list, setList] = useState([]);
     /* Use the map() function to render multiple Blocks! */
-    const posts = null; // TODO: edit this variable
+    const posts = list.map((color) => <Block color={color} caption={color}></Block>);
+
+    const update = (color) => {
+        setList(posts => [color, ...posts]);
+
+    }
 
     return (
         <div>
-            <Menu></Menu>
-
-            {/* Below is where all of your Blocks should render! */}
+            <Menu click={update}></Menu>
+            {/*Below is where all of your Blocks should render!*/}
             {posts}
         </div>
     );
 }
-
-export default Feed;
