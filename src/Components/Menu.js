bf250c1d62c6074bfd01ce4b135352062b61a79a
juliusritter <111826932+juliusritter@users.noreply.github.com>
2022-11-30 14:39:54 -0800

    Update Menu.js

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Color from "./Color"
+import Block from "./Color"
 /* Add any imports you think you might need here! */
 
-const Menu = () => { 
+const Menu = (props) => { 
 
     return (
       <div className="colorOptions">
-          {/* TODO */}
+        <Color color="red" handleClick={props.click}></Color>
+        <Color color="blue" handleClick={props.click}></Color>
+        <Color color="yellow" handleClick={props.click}></Color>
+        <Color color="Green" handleClick={props.click}></Color>
       </div>
     );
 }
+
+
+
+
+
+
+
+
 
 export default Menu;
